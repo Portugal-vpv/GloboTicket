600cf21e8541660e09f751d9be9ed52667e9aa1e
Victor Portugal de Vasconcellos <victorv@ciandt.com>
2021-04-13 11:02:15 -0300

    App,js startup

--- Home.js
+++ Home.js
@@ -1,9 +1,57 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { View, Text, Image, StyleSheet} from 'react-native';
 
-class Home extends Component {
-    render() {
-        return ();
-    }
+const Home = (props) => {
+    return (
+        <View style={styles.container}>
+            <Image
+                style={styles.globologo}
+                source={require('./images/globe.jpg')}
+            />
+            <Text style={styles.title}>Welcome to GloboTicket</Text>
+            <Text style={styles.subtitle}>{props.username}</Text>
+            <View style={styles.textcontainer}>
+                <Text style={styles.content}>{introText}</Text>
+            </View>
+        </View>
+    );
 }
+
+const introText = `This is just a random intro text. Lorem ipsum dolor mets
+ This is just a random intro text. Lorem ipsum dolor mets
+ This is just a random intro text. Lorem ipsum dolor mets
+ This is just a random intro text. Lorem ipsum dolor mets
+ This is just a random intro text. Lorem ipsum dolor mets
+ This is just a random intro text. Lorem ipsum dolor mets
+ This is just a random intro text. Lorem ipsum dolor mets`;
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: 20
+    },
+    textcontainer: {
+        textAlign: 'center',
+        paddingTop: 10
+    },
+    globologo: {
+        height: 140,
+        width: 150,
+    },
+    title: {
+        fontFamily: 'Ubuntu-Regular',
+        paddingTop: 20
+    },
+    subtitle: {
+        paddingTop: 5,
+        fontFamily: 'Ubuntu-Regular'
+    },
+    content: {
+        fontFamily: 'Ubuntu-Light',
+        fontWeight: '300',
+        width: 300
+    }
+});
 
 export default Home;
